105167a45844b0ede960efe7b20175fedb9316d5
Rcong <654846387@qq.com>
2017-03-12 17:46:40 +0800

    增加jsonp

--- 02.原生实现$.ajax/ajax.js
+++ 02.原生实现$.ajax/ajax.js
@@ -6,31 +6,46 @@ function ajax(options) {
         error: function(res) {}
     });
 
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            let json = JSON.stringify(xhr.responseText());
-            opt.success(json);
-        }
-        if (xhr.readyState === 4 && xhr.status === 404) {
-            opt.error();
-        }
-    }
-
     let dataStr = '';
     for (let key in opt.data) {
         dataStr += key + '=' + opt.data[key] + '&';
     }
     dataStr = dataStr.slice(0, dataStr.length - 1);
 
-    if (opt.type.toLowerCase() === 'get') {
-        xhr.open(opt.type, opt.url + '?' + dataStr, true);
-        xhr.send();
+    if (opt.type.toLowerCase() === 'jsonp') {
+
+        let script = document.createElement('script');
+        let success = function(res) {
+            opt.success(res);
+        }
+        script.src = opt.url + '?' + 'callback = success';
+        document.head.appendChild(script);
+        document.head.removeChild(script);
+
+    } else {
+
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                let json = JSON.stringify(xhr.responseText());
+                opt.success(json);
+            }
+            if (xhr.readyState === 4 && xhr.status === 404) {
+                opt.error();
+            }
+        }
+
+        if (opt.type.toLowerCase() === 'get') {
+            xhr.open(opt.type, opt.url + '?' + dataStr, true);
+            xhr.send();
+        }
+        if (opt.type.toLowerCase() === 'post') {
+            xhr.open(opt.type, opt.url, true);
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded"  );
+            xhr.send(dataStr);
+        }
+
     }
-    if (opt.type.toLowerCase() === 'post') {
-        xhr.open(opt.type, opt.url, true);
-        xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-        xhr.send(dataStr);
-    }
+
 
 }
